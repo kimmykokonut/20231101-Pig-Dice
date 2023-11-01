988db43076ae9add5c636fe22f84aec18aa6f1d0
kimmykokonut <kimmykokonut@gmail.com>
2023-11-01 09:48:20 -0700

    Test 3 pass. rollDice()
    Co-authored-by: Grant Abel <gtabel88@gmail.com>

--- pig-dice/js/scripts.js
+++ pig-dice/js/scripts.js
@@ -11,4 +11,33 @@ function Players(name, currentScore) {
 let playerOne = new Players("p1", 0);
 let playerTwo = new Players("p2", 0);
 
+// Describe rollDice()
 
+// Test: It will output a random number 1-6 and return result as turnScore
+// Code: rollDice(Math.floor(Math.random() * 6) + 1;)
+// Expected Output: number between 1 and 6
+
+// Test: It will give player option to roll again if result is 2-6
+// Code: rollDice()=> 3,
+// Expected Output: trigger rollDice() again
+
+// Test: It will give player option to "hold", pass turn to player 2
+// Code: rollDice()=> 
+// Expected Output: hold? y/n
+
+// Test: If player rolls 1, turnScore = 0, playerTurn switch
+// Code: rollDice()
+// Expected Output: 1, go to playerTurn()
+let turnScore = 0;
+
+function rollDice() {
+    let roll = Math.floor(Math.random() * 6) + 1; 
+    console.log(roll);
+    turnScore += roll;
+    return turnScore;   
+}
+// function turnScore() {
+//     let turnScore = 0;
+//     turnScore += roll;
+//     return turnScore;
+// }
